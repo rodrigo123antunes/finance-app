025d24945aa4fe3b9f94a3aed3036dd5c21aeb8d
“Rodrigo <rodrigobpzantunes2016@gmail.com>
2025-04-22 21:16:42 -0300

    chore: add any changes

---
+++ src/app/transactions/_components/type-badge.tsx
@@ -0,0 +1,34 @@
+import { Badge } from "@/components/ui/badge";
+import { Transaction, TransactionType } from "@prisma/client";
+import { CircleIcon } from "lucide-react";
+
+interface TransactionTypeBadgeProps {
+  transaction: Transaction;
+}
+
+const TransactionTypeBadge = ({ transaction }: TransactionTypeBadgeProps) => {
+  if (transaction.type === TransactionType.DEPOSIT) {
+    return (
+      <Badge className="bg-muted text-primary hover:bg-muted font-bold">
+        <CircleIcon className="fill-primary mr-2" size={10} />
+        Depósito
+      </Badge>
+    );
+  }
+  if (transaction.type === TransactionType.EXPENSE) {
+    return (
+      <Badge className="font bold bg-danger bg-opacity-10 text-danger">
+        <CircleIcon className="fill-danger mr-2" size={10} />
+        Despesa
+      </Badge>
+    );
+  }
+  return (
+    <Badge className="font bold bg-opacity-10 bg-white text-white">
+      <CircleIcon className="mr-2 fill-white" size={10} />
+      Investimento
+    </Badge>
+  );
+};
+
+export default TransactionTypeBadge;
---
+++ src/app/transactions/page.tsx
@@ -0,0 +1,18 @@
+import { db } from "@/_lib/prisma";
+import { transactionColumns } from "./_columns";
+import AddTransactionButton from "@/components/add-transaction-button";
+
+const TransactionsPage = async () => {
+  const transactions = await db.transaction.findMany({});
+  return (
+    <div className="space-y-6 p-6">
+      <div className="flex w-full items-center justify-between">
+        <h1 className="text-2xl font-bold">Transações</h1>
+        <AddTransactionButton />
+      </div>
+      <DataTable columns={transactionColumns} data={transactions} />
+    </div>
+  );
+};
+
+export default TransactionsPage;
